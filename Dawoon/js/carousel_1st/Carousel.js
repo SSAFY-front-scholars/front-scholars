5d838ee27e630ee36e54a3f9b4dcee166ebc404d
kimdw524 <kimdw524@naver.com>
2024-02-12 04:30:39 +0900

    feat: 자동 스킵 타이머 추가

--- Dawoon/js/carousel_1st/Carousel.js
+++ Dawoon/js/carousel_1st/Carousel.js
@@ -19,24 +19,31 @@ document.querySelectorAll('.carousel').forEach(($e) => {
 
     $e.querySelector(`.carousel-indicator:nth-child(${slide + 1})`)?.classList.add('selected');
 
-    const currentItem = $e.querySelector('.carousel-inner .carousel-item.selected');
-    const nextItem = $e.querySelector(`.carousel-inner .carousel-item:nth-child(${slide + 1})`);
+    const $currentItem = $e.querySelector('.carousel-inner .carousel-item.selected');
+    const $nextItem = $e.querySelector(`.carousel-inner .carousel-item:nth-child(${slide + 1})`);
+    const $progress = $e.querySelector('.carousel-timer-progressbar-child');
 
-    currentItem?.classList.remove('selected');
-    currentItem?.classList.add(toRight ? 'right' : 'left');
-    currentItem.ontransitionend = () => {
-      currentItem?.classList.remove('left', 'right');
-      nextItem?.classList.remove('left', 'right');
-
+    $currentItem?.classList.remove('selected');
+    $currentItem?.classList.add(toRight ? 'right' : 'left');
+    $currentItem.ontransitionend = () => {
+      $currentItem?.classList.remove('left', 'right');
+      $nextItem?.classList.remove('left', 'right');
       preventTransition = false;
+      $progress?.classList.add('active');
     };
 
-    nextItem?.classList.add('selected');
-    nextItem?.classList.add(toRight ? 'right' : 'left');
+    $nextItem?.classList.add('selected');
+    $nextItem?.classList.add(toRight ? 'right' : 'left');
+
+    $e.querySelector('.carousel-timer-container span.current').innerHTML = `0${slide + 1}`.slice(
+      -2,
+    );
+    $progress.classList.remove('active');
   };
 
   const init = () => {
     const $container = $e.querySelector('.carousel-indicator-container');
+    const { length } = $e.querySelectorAll('.carousel-inner .carousel-item');
     $e.querySelector('.carousel-inner .carousel-item:nth-child(1)')?.classList.add('selected');
 
     $e.querySelectorAll('.carousel-inner .carousel-item').forEach((element, index) => {
@@ -49,6 +56,14 @@ document.querySelectorAll('.carousel').forEach(($e) => {
       $container.append($indicator);
     });
     $e.querySelector('.carousel-indicator:nth-child(1)')?.classList.add('selected');
+    $e.querySelector('.carousel-timer-container span.current').innerHTML = '01';
+    $e.querySelector('.carousel-timer-container span.last').innerHTML = `0${length}`.slice(-2);
+
+    const $progress = $e.querySelector('.carousel-timer-progressbar-child');
+    $progress.classList.add('active');
+    $progress?.addEventListener('transitionend', () => {
+      setSlide(slide + 1);
+    });
   };
 
   $e.querySelector('.prev')?.addEventListener('click', () => {
