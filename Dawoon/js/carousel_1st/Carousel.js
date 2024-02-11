daeecaa81fa3d1f9d2f0775564750d902e909cf4
kimdw524 <kimdw524@naver.com>
2024-02-12 03:41:36 +0900

    feat: indicator 추가

--- Dawoon/js/carousel_1st/Carousel.js
+++ Dawoon/js/carousel_1st/Carousel.js
@@ -2,13 +2,10 @@ document.querySelectorAll('.carousel').forEach(($e) => {
   let preventTransition = false;
   let slide = 0;
 
-  const init = () => {
-    $e.querySelector('.carousel-inner .carousel-item:nth-child(1)')?.classList.add('selected');
-  };
-
   const setSlide = (value) => {
     if (preventTransition) return;
     const toRight = slide < value;
+    $e.querySelector(`.carousel-indicator:nth-child(${slide + 1})`)?.classList.remove('selected');
     slide = value;
     preventTransition = true;
 
@@ -19,6 +16,8 @@ document.querySelectorAll('.carousel').forEach(($e) => {
     } else if (value >= length) {
       slide = 0;
     }
+
+    $e.querySelector(`.carousel-indicator:nth-child(${slide + 1})`)?.classList.add('selected');
 
     const currentItem = $e.querySelector('.carousel-inner .carousel-item.selected');
     const nextItem = $e.querySelector(`.carousel-inner .carousel-item:nth-child(${slide + 1})`);
@@ -34,6 +33,21 @@ document.querySelectorAll('.carousel').forEach(($e) => {
 
     nextItem?.classList.add('selected');
     nextItem?.classList.add(toRight ? 'right' : 'left');
+  };
+
+  const init = () => {
+    const $container = $e.querySelector('.carousel-indicator-container');
+    $e.querySelector('.carousel-inner .carousel-item:nth-child(1)')?.classList.add('selected');
+
+    $e.querySelectorAll('.carousel-inner .carousel-item').forEach((element, index) => {
+      const $indicator = document.createElement('button');
+      $indicator.classList.add('carousel-indicator');
+      $indicator.addEventListener('click', () => {
+        setSlide(index);
+      });
+      $container.append($indicator);
+    });
+    $e.querySelector('.carousel-indicator:nth-child(1)')?.classList.add('selected');
   };
 
   $e.querySelector('.prev')?.addEventListener('click', () => {
