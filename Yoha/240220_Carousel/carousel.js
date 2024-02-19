462a3f99b67638528567272af0f75d132c040aed
yh-project <yoha6865@gmail.com>
2024-02-19 23:49:24 +0900

    feat: add detail annotation

--- Yoha/240220_Carousel/carousel.js
+++ Yoha/240220_Carousel/carousel.js
@@ -1,60 +1,71 @@
+/**
+ * slides : 슬라이드들
+ * indicators : 인디케이터들
+ * prevBtn : 이전 버튼
+ * nextBtn : 다음 버튼
+ */
 const slides = document.querySelector(".carousel-slide");
 const indicators = document.querySelectorAll(".indicator");
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
-let currentIndex = 0;
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+/**
+ * curIdx : 현재 슬라이드 index
+ * intervalId : 자동 슬라이드 id
+ */
+let curIdx = 0;
 let intervalId;
 
-// Set initial active indicator
-indicators[currentIndex].classList.add("active");
-
-// Event listener for prev button
-prevButton.addEventListener("click", () => {
-  currentIndex =
-    (currentIndex - 1 + slides.children.length) % slides.children.length;
-  updateCarousel();
-  resetInterval();
-});
-
-// Event listener for next button
-nextButton.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % slides.children.length;
-  updateCarousel();
-  resetInterval();
-});
-
-// Event listeners for indicators
-indicators.forEach((indicator, index) => {
-  indicator.addEventListener("click", () => {
-    currentIndex = index;
-    updateCarousel();
-    resetInterval();
-  });
-});
-
-// Auto slide every 3 seconds
-startInterval();
-
-function startInterval() {
+/**
+ * startInteval : 자동 슬라이딩 시작
+ * restartInterval : 자동 슬라이딩 초기화
+ * updateCarousel : carousel 슬라이드 최신화
+ */
+const startInterval = () => {
   intervalId = setInterval(() => {
-    currentIndex = (currentIndex + 1) % slides.children.length;
+    curIdx = (curIdx + 1) % slides.children.length;
     updateCarousel();
   }, 3000);
-}
+};
 
-function resetInterval() {
+const restartInterval = () => {
   clearInterval(intervalId);
   startInterval();
-}
+};
 
-// Update carousel slide based on currentIndex
-function updateCarousel() {
-  slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+const updateCarousel = () => {
+  slides.style.transform = `translateX(-${curIdx * 100}%)`;
   indicators.forEach((indicator, index) => {
-    if (index === currentIndex) {
-      indicator.classList.add("active");
-    } else {
-      indicator.classList.remove("active");
-    }
+    if (index === curIdx) indicator.classList.add("active");
+    else indicator.classList.remove("active");
   });
-}
+};
+
+// 이미지 자동슬라이드 시작
+startInterval();
+
+// 시작 슬라이드 0번으로
+indicators[curIdx].classList.add("active");
+
+// 이전 버튼
+prevBtn.addEventListener("click", () => {
+  curIdx = (curIdx - 1 + slides.children.length) % slides.children.length;
+  updateCarousel();
+  restartInterval();
+});
+
+// 다음 버튼
+nextBtn.addEventListener("click", () => {
+  curIdx = (curIdx + 1) % slides.children.length;
+  updateCarousel();
+  restartInterval();
+});
+
+// 인디케이터 클릭하면 해당 이미지로
+indicators.forEach((indicator, idx) => {
+  indicator.addEventListener("click", () => {
+    curIdx = idx;
+    updateCarousel();
+    restartInterval();
+  });
+});
